f8199a70a7fb0457be626e62ba8253e4177d7bd9
Vitaliy Stepanenko <marshallmk94@gmail.com>
2019-12-12 12:55:38 +0200

    Update UI/UX job description and profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationStart } from '@angular/router';
 import { AuthService } from './services/auth.service';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  public backStatus: boolean;
   constructor(
     public router: Router,
     private authService: AuthService,
+    private location: Location
   ) { }
 
   ngOnInit() {
+    this.router.events
+      .pipe()
+      .subscribe(
+        res => {
+          console.log(res);
+          if (res instanceof NavigationStart) {
+            console.log(res);
+            if (res.url === '/profile') {
+              this.backStatus = true;
+            } else {
+              this.backStatus = false;
+            }
+          }
+        }
+      );
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+  public backRouter = () => {
+    this.location.back();
   }
   public goToComponent = (name: string, nav: any) => {
     this.router.navigate([`${name}`]);
