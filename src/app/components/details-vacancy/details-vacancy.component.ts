0d9106fcda75a8767c188617ac1399ecbc52bc58
Vitaliy Stepanenko <marshallmk94@gmail.com>
2020-04-28 13:54:49 +0300

    Create new design job vacancy details

--- src/app/components/details-vacancy/details-vacancy.component.ts
+++ src/app/components/details-vacancy/details-vacancy.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PrivacyPolicyComponent } from '../modal/privacy-policy/privacy-policy.component';
+import { MatDialog } from '@angular/material';
+import { GlobalErrorService } from 'src/app/services/global-error-service';
+import { ApplicationService } from 'src/app/services/application-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-details-vacancy',
@@ -8,10 +13,39 @@ import { Component, OnInit, Input } from '@angular/core';
 export class DetailsVacancyComponent implements OnInit {
 
   @Input() vacancyData;
+  @Input() newVacancyDesign;
 
-  constructor() { }
+  public privacyPolicy;
+
+  constructor(
+    public matDialog: MatDialog,
+    public applicationService: ApplicationService,
+    public router: Router,
+    private globalErrorService: GlobalErrorService
+  ) { }
 
   ngOnInit() {
   }
 
+  public openPrivacyDialog = () => {
+    this.matDialog.open(PrivacyPolicyComponent, { panelClass: 'privacy-policy-dialog' });
+  }
+
+  public applyVacancy = (id: string) => {
+    const jobApply$ = this.applicationService.jobApply(id);
+    jobApply$
+      .pipe()
+      .subscribe(
+        res => {
+          this.applicationService.removeJobId();
+          this.router.navigate([`/apply-thanks/${id}/keep/true`]);
+        },
+        err => {
+          console.log('[ ERROR APPLY JOB ]', err);
+          this.applicationService.removeJobId();
+          this.globalErrorService.handleError(err);
+        },
+        () => console.log('[ DONE APPLY JOB ]')
+      );
+  }
 }
