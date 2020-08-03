170ad7576bd51b9868a170a1e2932a3247f16180
Vitaliy Stepanenko <marshallmk94@gmail.com>
2020-08-03 11:11:12 +0300

    Fix accordion animation and component view

--- src/app/animations/router-animations.ts
+++ src/app/animations/router-animations.ts
@@ -1,35 +1,26 @@
-import { trigger, animate, transition, style, query } from '@angular/animations';
+import { trigger, animate, transition, style, state } from '@angular/animations';
 
 export const fadeAnimation =
-
   trigger('fadeAnimation', [
-
-    transition('* => *', [
-
-      query(':enter',
-        [
-          style({ opacity: 0, transform: 'scale(.8)' })
-        ],
-        { optional: true }
-      ),
-
-      query(':leave',
-        [
-          style({ opacity: 1, transform: 'scale(1)' }),
-          animate('.5s cubic-bezier(.42, .97, .52, 1.49)', style({ opacity: 0, transform: 'scale(.8)' }))
-        ],
-        { optional: true }
-      ),
-
-      query(':enter',
-        [
-          style({ opacity: 0, transform: 'scale(.8)' }),
-          animate('.5s cubic-bezier(.42, .97, .52, 1.49)', style({ opacity: 1, transform: 'scale(1)' }))
-        ],
-        { optional: true }
-      )
-
-    ])
-
+    state(
+      'void',
+      style({
+        opacity: 0,
+        transform: 'scale(0.8)'
+      })
+    ),
+    state(
+      'active',
+      style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })
+    ),
+    state(
+      'noActive',
+      style({})
+    ),
+    transition('void => active', animate('.5s cubic-bezier(.42, .97, .52, 1.49)')),
+    transition('* => void', animate('.5s cubic-bezier(.42, .97, .52, 1.49)'))
   ]);
 // cubic-bezier(.42, .97, .52, 1.49)
